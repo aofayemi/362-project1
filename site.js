01c87b131e0a318395cd90a80212183faeead5bf
Toby <aofayemi@gmail.com>
2019-02-22 14:34:59 -0600

    Add email validate element

--- site.js
+++ site.js
@@ -11,8 +11,47 @@
     var phone=document.querySelector('#telephone');
     var signup=document.querySelector('#signup');
     var submit=.document.querySelector('#submit');
+    var ph=false;
+    var em=false;
 
     console.log('OMG the DOM is loaded!!!!');
+    email.addEventListener('focus', focus);
+    phone.addEventListener('focus', focus);
+    submit.addEventListener('focus', focus);
+    signup.addEventListener('focus', focus);
 
-  })
-});
+    //Disable signup button if either email or phone number is filled
+    function check(){
+      if (ph === true || em === true){
+
+
+      signup.removeAttribute('disabled')
+      submit.removeAttribute('disabled');}
+      else{
+        submit.setAttribute('disabled', 'disabled')
+        signup.setAttribute('disabled', 'disabled');}
+      }
+    }
+
+
+  });
+
+    //check if email is valid
+    function checkEmail(email){
+     var em = /^[^@]+@[^@]+$/g;
+      //remove whitespace
+      email = email.replace(/\s/g, '');
+      if (email.length >= 1 && this.value.match(email)) {
+        em = true;}
+        else {
+          em = false;
+        }
+
+      });
+
+      //Check for valid phone Number
+      phone.addEventListener('input', function(){
+
+      })
+
+})();
