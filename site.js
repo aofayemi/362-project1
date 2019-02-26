77c98023a93d4a09b5290072e7888200e32720ee
Toby <aofayemi@gmail.com>
2019-02-25 19:20:30 -0600

    Add phone validity and fuction to give professional look

--- site.js
+++ site.js
@@ -6,14 +6,38 @@
     return;
   }
 
+
+
+
+
+
+      //Email validity function
+      function checkEmail(email){
+       var em = /^[^@]+@[^@]+$/g;
+        //remove whitespace
+        email = email.replace(/\s/g, '');
+        if (email.length >= 1 && this.value.match(email)) {
+          em = true;}
+          else {
+            em = false;
+          }
+
+        });
+
+        //Check for valid phone Number
+        function validate_us_phone(value){
+          var phone_number = trip_us_country_code(clean_nonnumber(value));
+          return validate(phone_number.length,eq,10);
+        }
+
+
+
   document.addEventListener('DOMContentLoaded', function(){
     //select the necessary elements from the DOM
     var signup_input = document.querySelector('signup-form');
     var email_input=document.querySelector('#email');
     var telephone_input=document.querySelector('#telephone');
     var signup_submit=document.querySelector('#signup');
-    var ph=false;
-    var em=false;
 
 
     //Disable signup button if either email or phone number is filled.
@@ -29,23 +53,5 @@
 
 
   });
-
-    //Email validity function
-    function checkEmail(email){
-     var em = /^[^@]+@[^@]+$/g;
-      //remove whitespace
-      email = email.replace(/\s/g, '');
-      if (email.length >= 1 && this.value.match(email)) {
-        em = true;}
-        else {
-          em = false;
-        }
-
-      });
-
-      //Check for valid phone Number
-      phone.addEventListener('input', function(){
-
-      })
 
 })();
